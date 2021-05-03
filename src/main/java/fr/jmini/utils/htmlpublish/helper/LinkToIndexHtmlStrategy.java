25c84e63a59628d344410d85c52e7a2c3cf85acb
Jeremie Bresson <jeremie.bresson@unblu.com>
2021-05-03 12:09:04 +0200

    Add LinkToIndexHtmlStrategy option
    Fixes #9

---
+++ src/main/java/fr/jmini/utils/htmlpublish/helper/LinkToIndexHtmlStrategy.java
@@ -0,0 +1,14 @@
+package fr.jmini.utils.htmlpublish.helper;
+
+public enum LinkToIndexHtmlStrategy {
+
+    /**
+     * Link to the parent folder of the `index.html` file
+     */
+    TO_PARENT_FOLDER,
+
+    /**
+     * Explicit link to the `index.html` file
+     */
+    TO_FILE
+}
